834bfba722fc8e9947f520390185267220498019
Piri-kara <hpt_q@yahoo.co.jp>
2020-02-25 17:30:34 +0900

    算術代入演算子

--- Sample13.scala
+++ Sample13.scala
@@ -1,6 +1,23 @@
 object Sample13{
   def main(arg: Array[String]){
     //算術代入演算子
+    //左辺の変数に右辺の値との算術結果を代入する
+    var x=7
     
+    x+=3
+    println("x="+x)
+
+    x-=2
+    println("x="+x)
+
+    x*=3
+    println("x="+x)
+
+    x/=2
+    println("x="+x)
+
+    x%=5
+    println("x="+x)
+
   }
 }
