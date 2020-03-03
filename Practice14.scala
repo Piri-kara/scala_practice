7af737849fe72dd64ef7deaac8dcf61866d495b0
Piri-kara <hpt_q@yahoo.co.jp>
2020-03-03 14:52:44 +0900

    オーバーライドの演習問題

---
+++ Practice14.scala
@@ -0,0 +1,25 @@
+object Practice14{
+  def main(args: Array[String]){
+    val b=new Bicycle
+    val mb=new MotorBicycle
+    b.turn()
+    b.move()
+    mb.turn()
+    mb.move()
+  }
+}
+
+class Bicycle{
+  def turn(){
+    println("ハンドルを切ります。")
+  }
+  def move(){
+    println("ペダルを漕いで進みます。")
+  }
+}
+
+class MotorBicycle extends Bicycle{
+  override def move(){
+    println("エンジンを動力に進みます。")
+  }
+}
